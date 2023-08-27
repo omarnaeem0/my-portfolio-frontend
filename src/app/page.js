1a1f8d8be3a6aecf9abd3dea07f108ec69dbb396
omarnaeem0 <omar_naeem@live.com>
2023-08-27 13:20:03 +0500

    first section complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
 import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
-import cx from 'classnames';
 import Image from 'next/image';
 import profilePic from '../../public/profile.png'
 import { BsFillMoonStarsFill } from 'react-icons/bs';
@@ -29,17 +28,17 @@ export default function Home() {
           </nav>
           <div className='flex justify-evenly flex-col lg:flex-row items-center'>
             <div className='lg:max-w-lg'>
-              <div className='text-center p-4 md:p-10'>
-                <h2 className='text-4xl lg:text-5xl py-2 text-teal-500 font-medium '>Omar Naeem</h2>
+              <div className='text-center lg:p-10'>
+                <h2 className='text-4xl lg:text-5xl py-2 lg:py-4 text-teal-500 font-medium '>Omar Naeem</h2>
                 <h3 className='text-xl lg:text-2xl py-2'>Front-End React and React-Native Developer.</h3>
                 <p className='textsm lg:text-md py-5 leading-6 lg:leading-8 text-gray-800'>Frontend Developer providing services for developing Web applications using React and Android/iOS applications using React Native</p>
               </div>
-              <div className='text-3xl lg:text-4xl flex justify-center gap-12 lg:gap-16 py-3 text-gray-600'>
+              <div className='text-3xl lg:text-4xl flex justify-center gap-10 lg:gap-12 py-3 text-gray-600'>
                 <AiFillLinkedin />
                 <AiFillGithub />
               </div>
             </div>
-            <div className='relative bg-gradient-to-r from-cyan-500 to-teal-500 rounded-full overflow-hidden w-60 lg:w-80 h-60 lg:h-80 mt-20'>
+            <div className='relative bg-gradient-to-r from-cyan-500 to-teal-500 rounded-full overflow-hidden w-60 lg:w-80 h-60 lg:h-80 mt-10 lg:mt-20'>
               <Image src={profilePic} />
             </div>
           </div>
