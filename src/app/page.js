6d614c2e08dedb58fb069b624fef5aea27b1f851
omarnaeem0 <omar_naeem@live.com>
2023-08-29 17:05:25 +0500

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -49,10 +49,10 @@ export default function Home() {
               <h1 className='text-2xl font-burtons text-gray-600 dark:text-gray-400'></h1>
               <ul className="flex items-center">
                 <li>
-                  <BsFillMoonStarsFill className='text-2xl cursor-pointer text-gray-600 dark:text-gray-400' onClick={setDarkMode} />
+                  <BsFillMoonStarsFill className='text-2xl cursor-pointer text-gray-600 dark:text-gray-400 hover:text-blue-500 hover:dark:text-blue-500' onClick={setDarkMode} />
                 </li>
                 <li >
-                  <a download="" href="Omar-Naeem-CV.pdf" className="bg-gradient-to-t lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-indigo-400 dark:to-indigo-500 px-4 py-2 rounded-md ml-8 text-white">Resume</a>
+                  <a download="" href="Omar-Naeem-CV.pdf" className="bg-gradient-to-t lg:bg-gradient-to-r from-blue-500 to-violet-500 px-4 py-2 rounded-md ml-8 text-white">Resume</a>
                 </li>
               </ul>
             </nav>
