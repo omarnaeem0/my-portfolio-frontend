a1bdd5874043837f45aea30c652ea188ac70ed95
omarnaeem0 <omar_naeem@live.com>
2023-08-29 23:04:12 +0500

    portfolio basic template added

--- src/app/constants.js
+++ src/app/constants.js
@@ -129,5 +129,34 @@ export default {
         src: JiraIcon,
       },
     ]
+  },
+  portfolio: {
+    title: 'Portfolio',
+    description: 'Some of my recent projects',
+    projects: [
+      {
+        name: 'Intelligize',
+        content: `Intelligize is a regtech/legaltech/fintech project. It is the leading provider of best-in-class content, exclusive news collections, regulatory insights, and powerful
+        analytical tools for compliance and transactional professionals. It offers a web-based research platform that ensures law firms, accounting firms, corporations and other
+        organizations stay compliant with government regulations, build stronger deals and agreements, and deliver value to their shareholders and clients.
+        It's like an advanced search engine that parses documents submitted by companies on SEC and allows users to view different types of information related to the
+        documents they are interested in, it can be in the form of dashboards, tables, graphs and so much more.
+        There are several apps within this project and each app has a different way of presenting different kinds of information. Each app was introduced as a new feature and
+        there are several new advancements still going on in the project. The technologies used to build this project are ReactJS, JavaScript along with Redux and Redux-Saga.
+        It's a huge project and is been in development since 2016.`
+      },
+      {
+        name: 'SUNNDIO',
+        content: `SUNNDIO is a healthtech, AI based web application developed with ReactJS, designed to be responsive and accessible on both mobile devices and desktops. Its primary
+        purpose is to evaluate patients by gathering information about their pain area, pain behaviour, and symptoms. Based on this evaluation, the application provides
+        personalized diagnoses and offers various treatment exercises, including guides and videos, to aid users in their recovery and improve their overall health.`
+      },
+      {
+        name: 'GREENER',
+        content: `A foodtech/healthtech AI based React-Native app for Android and iOS both that can help users manage kitchen inventory, orders groceries, recommend recipes from
+        available inventory and connects with kitchen appliances in the simplest manner. Main goal was to increase automation of grocery orders and decline food waste by 90
+        percent. And user goal to receive assistance in managing Kitchen in a personalised and convenient way.`
+      }
+    ]
   }
 }
--- src/app/sections/PortfolioSection.js
+++ src/app/sections/PortfolioSection.js
@@ -1,12 +1,31 @@
+import { BsFillCaretLeftFill, BsFillCaretRightFill } from 'react-icons/bs';
+import constants from '../constants';
+
 export default function PortfolioSection() {
   return (
     <div className='text-center lg:p-10 py-16'>
-      <h3 className='text-2xl lg:text-3xl py-2 font-semibold text-gray-600 dark:text-white'>Portfolio</h3>
+      <h3 className='text-2xl lg:text-3xl py-2 font-semibold text-gray-600 dark:text-white'>{constants.portfolio.title}</h3>
       <p className='text-md lg:text-lg py-2 leading-6 lg:leading-8 text-gray-800 dark:text-white'>
-        {'Some of my recent projects'}
+        {constants.portfolio.description}
       </p>
-      <div className='flex flex-row justify-center gap-12 flex-wrap py-6 px-10 max-w-5xl m-auto mt-4'>
-
+      <div className='flex flex-row items-center gap-12 flex-wrap py-6 px-10 max-w-5xl m-auto mt-4'>
+        <div className='text-gray-600 dark:text-white w-10 h-40 flex justify-center items-center'>
+          <BsFillCaretLeftFill className='text-xl lg:text-2xl' />
+        </div>
+        <div className='flex flex-1 flex-row items-center justify-between'>
+          {constants.portfolio.projects.map(project => (
+            <div className='border-solid rounded-md overflow-clip xl:w-60'>
+              <div className='bg-white xl:h-40 xl:w-60 pb-1'></div>
+              <div className='text-left xl:h-40 bg-white/20 p-2'>
+                <h3 className='text-gray-900 dark:text-white font-bold text-md p-1'>{project.name}</h3>
+                <p className='text-gray-900 dark:text-white text-sm p-1 text-ellipsis'>{project.content}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className='text-gray-600 dark:text-white w-10 h-40 flex justify-center items-center'>
+          <BsFillCaretRightFill className='text-xl lg:text-2xl' />
+        </div>
       </div>
     </div>
   )
