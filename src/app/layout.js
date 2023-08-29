712b251103bbd2c6b27e998c3afeccf0cc6ab4af
omarnaeem0 <omar_naeem@live.com>
2023-08-29 12:49:57 +0500

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className='overflow-hidden'>
+    <html lang="en">
       <body className={`${inter.className}`}>{children}</body>
     </html>
   )
