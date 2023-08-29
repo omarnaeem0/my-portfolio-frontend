b41719f49165fe373fe083e3c8ca5cad221b6628
omarnaeem0 <omar_naeem@live.com>
2023-08-29 11:09:26 +0500

    temp commit

--- src/app/page.js
+++ src/app/page.js
@@ -127,6 +127,15 @@ export default function Home() {
                 <Image src={JiraIcon} className='h-14 lg:h-16 w-auto' />
               </div>
             </div>
+            {/* <div className='text-center lg:p-10 py-16'>
+              <h3 className='text-2xl lg:text-3xl py-2 font-semibold text-gray-600 dark:text-white'>Portfolio</h3>
+              <p className='text-md lg:text-lg py-2 leading-6 lg:leading-8 text-gray-800 dark:text-white'>
+                {'My most recent work'}
+              </p>
+              <div className='flex flex-row justify-center gap-12 flex-wrap py-6 px-10 max-w-5xl m-auto mt-4'>
+                
+              </div>
+            </div> */}
           </section>
         </main>
       </div>
