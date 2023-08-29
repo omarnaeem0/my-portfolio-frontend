6fb6de7947332861cb34bf05d707a04d2db545f2
omarnaeem0 <omar_naeem@live.com>
2023-08-29 12:29:18 +0500

    github link added

--- src/app/page.js
+++ src/app/page.js
@@ -66,7 +66,9 @@ export default function Home() {
                   <a href="https://www.linkedin.com/in/omar-naeem-janjua" target="_blank">
                     <AiFillLinkedin />
                   </a>
-                  <AiFillGithub />
+                  <a href="https://github.com/omarnaeem0" target="_blank">
+                    <AiFillGithub />
+                  </a>
                 </div>
               </div>
               <div className='relative bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-80 h-60 lg:h-80 my-8 lg:my-10'>
@@ -140,14 +142,14 @@ export default function Home() {
         </main>
       </div>
       <div className='bg-white dark:bg-gray-900 w-screen h-screen absolute overflow-clip' >
-        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 top-10 right-10 scale-150'/>
-        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 top-4 right-4'/>
-        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 hidden md:block top-24 left-44 lg:top-10 lg:left-20 scale-50'/>
-        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 hidden md:block top-44 left-24 lg:-top-10 lg:left-0 scale-75'/>
-        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 bottom-0 left-10 scale-150'/>
-        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 -bottom-6 left-4'/>
-        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 bottom-64 right-24 lg:-bottom-32 lg:right-0 scale-75'/>
-        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 bottom-80 right-40 lg:-bottom-12 lg:right-20 scale-50'/>
+        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 top-10 right-10 scale-150' />
+        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 top-4 right-4' />
+        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 hidden md:block top-24 left-44 lg:top-10 lg:left-20 scale-50' />
+        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 hidden md:block top-44 left-24 lg:-top-10 lg:left-0 scale-75' />
+        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 bottom-0 left-10 scale-150' />
+        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 -bottom-6 left-4' />
+        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 bottom-64 right-24 lg:-bottom-32 lg:right-0 scale-75' />
+        <div className='absolute bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-96 h-60 lg:h-96 opacity-20 bottom-80 right-40 lg:-bottom-12 lg:right-20 scale-50' />
       </div>
     </div>
   )
