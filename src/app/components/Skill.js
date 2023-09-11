07cccca65a69f03f2f38e3391cfd9a2c0417106e
omarnaeem0 <omar_naeem@live.com>
2023-09-11 12:16:56 +0500

    lazy loading added

--- src/app/components/Skill.js
+++ src/app/components/Skill.js
@@ -4,7 +4,7 @@ export default function Skill(props) {
   const { title, src, className } = props;
   return (
     <>
-      <Image data-tooltip-target={title} src={src} className={className || `h-14 lg:h-16 w-auto`} alt=""/>
+      <Image data-tooltip-target={title} src={src} className={className || `h-14 lg:h-16 w-auto`} alt="" loading='lazy'/>
       <div id={title} role="tooltip" data-popper-placement="top" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 dark:text-white bg-white dark:bg-gray-700 borderrounded-lg shadow-sm opacity-0 tooltip">
         {title}
         <div className="tooltip-arrow" data-popper-arrow></div>
--- src/app/sections/IntroductionSection.js
+++ src/app/sections/IntroductionSection.js
@@ -20,7 +20,7 @@ export default function IntroductionSection() {
         </div>
       </div>
       <div className='relative bg-gradient-to-b lg:bg-gradient-to-r from-blue-400 dark:from-blue-500 to-violet-500 dark:to-violet-600 image-frame overflow-hidden w-60 lg:w-80 h-60 lg:h-80 my-8 lg:my-10'>
-        <Image src={introduction.image} alt="" />
+        <Image src={introduction.image} alt="" loading='lazy'/>
       </div>
     </div>
   )
