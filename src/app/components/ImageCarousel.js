58f2b13110e2f7bae2025813ca929dcd593dc5bc
omarnaeem0 <omar_naeem@live.com>
2023-09-12 12:24:14 +0500

    potfolio carousal should take full width

--- src/app/components/ImageCarousel.js
+++ src/app/components/ImageCarousel.js
@@ -1,48 +1,91 @@
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import Image from "next/image";
+import { useEffect, useState, useCallback } from "react";
 
 export default function ImageCarousel(props) {
   const { images } = props;
   const [selected, setSelected] = useState(0);
-  const onNext = () => {
+  const onNext = useCallback(() => {
     if (selected === images.length - 1) {
       setSelected(0);
     } else {
       setSelected(selected + 1);
     }
-  }
-  const onPrevious = () => {
+  }, [setSelected, selected, images]);
+  const onPrevious = useCallback(() => {
     if (selected === 0) {
       setSelected(images.length - 1);
     } else {
       setSelected(selected - 1);
     }
-  }
+  }, [setSelected, selected, images]);
   useEffect(() => {
     const timer = setInterval(() => {
       onNext();
     }, 2000);
     return () => clearInterval(timer);
-  }, [onNext])
+  }, [onNext]);
   return (
-    <div className="relative bg-white/50 dark:bg-black/30 rounded-3xl h-80 md:h-full w-full md:w-72 lg:w-96 xl:w-128 md:mb-0">
+    <div className="relative bg-white/50 dark:bg-black/30 rounded-3xl h-80 lg:h-full w-full lg:w-128 xl:w-128 lg:mb-0">
       <div className="relative overflow-hidden rounded-lg h-full">
-        {
-          images.map((image, index) => (
-            <Image key={index} src={image} className={`max-h-full w-auto m-auto absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 transition-all duration-500 p-4 ${selected === index ? 'opacity-100' : `${selected > index ? '-' : ''}translate-x-full opacity-0`}`} alt="" loading='lazy' />
-          ))
-        }
+        {images.map((image, index) => (
+          <Image
+            key={index}
+            src={image}
+            className={`max-h-full w-auto m-auto absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 transition-all duration-500 p-4 ${
+              selected === index
+                ? "opacity-100"
+                : `${selected > index ? "-" : ""}translate-x-full opacity-0`
+            }`}
+            alt=""
+            loading="lazy"
+          />
+        ))}
       </div>
-      <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={onPrevious}>
-        <svg className="w-4 h-4 text-indigo-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
-        </svg>
-      </button>
-      <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={onNext}>
-        <svg className="w-4 h-4 text-indigo-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
-        </svg>
-      </button>
+      <div className="absolute bottom-5 flex flex-row w-full justify-center items-center ">
+        <button
+          type="button"
+          className="z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          onClick={onPrevious}
+        >
+          <svg
+            className="w-4 h-4 text-indigo-500"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 1 1 5l4 4"
+            />
+          </svg>
+        </button>
+        <p className="text-indigo-500 text-sm lg:text-sm overflow-clip">
+          {selected + 1} / {images.length}
+        </p>
+        <button
+          type="button"
+          className="z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          onClick={onNext}
+        >
+          <svg
+            className="w-4 h-4 text-indigo-500"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 9 4-4-4-4"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
--- src/app/sections/PortfolioSection.js
+++ src/app/sections/PortfolioSection.js
@@ -1,58 +1,96 @@
-import { useState } from 'react';
-import { HiExternalLink } from 'react-icons/hi';
-import portfolio from '../constants/portfolio';
-import ImageCarousel from '../components/ImageCarousel';
+import { useState, useCallback } from "react";
+import { HiExternalLink } from "react-icons/hi";
+import portfolio from "../constants/portfolio";
+import ImageCarousel from "../components/ImageCarousel";
 
 export default function PortfolioSection() {
   const [selected, setSelected] = useState(0);
-  const onNext = () => {
+  const onNext = useCallback(() => {
     if (selected === portfolio.projects.length - 1) {
       setSelected(0);
     } else {
       setSelected(selected + 1);
     }
-  }
-  const onPrevious = () => {
+  }, [setSelected, selected]);
+  const onPrevious = useCallback(() => {
     if (selected === 0) {
       setSelected(portfolio.projects.length - 1);
     } else {
       setSelected(selected - 1);
     }
-  }
+  }, [setSelected, selected]);
   return (
-    <div className='text-center lg:p-2 py-16'>
-      <h3 className='text-2xl lg:text-3xl py-2 font-semibold text-gray-600 dark:text-white'>{portfolio.title}</h3>
-      <p className='text-md lg:text-lg py-2 leading-6 lg:leading-8 text-gray-800 dark:text-white'>
+    <div className="text-center lg:p-2 py-16">
+      <h3 className="text-2xl lg:text-3xl py-2 font-semibold text-gray-600 dark:text-white">
+        {portfolio.title}
+      </h3>
+      <p className="text-md lg:text-lg py-2 leading-6 lg:leading-8 text-gray-800 dark:text-white">
         {portfolio.description}
       </p>
-      <div className='flex flex-row items-center flex-wrap py-10 max-w-5xl mx-auto mt-4'>
+      <div className="flex flex-row items-center flex-wrap py-10 max-w-5xl mx-auto">
         <div className="relative w-full">
-          <div className="relative h-144 md:h-96 xl:h-128 overflow-hidden rounded-lg">
+          <div className="relative h-144 lg:h-96 xl:h-128 overflow-hidden rounded-lg">
             {portfolio.projects.map((project, index) => (
-              <div key={project.name} className={`h-full absolute px-14 lg:px-16 transition-all duration-500 ${selected === index ? 'opacity-100' : `${selected > index ? '-' : ''}translate-x-full opacity-0`}`}>
-                <div className='flex flex-col md:flex-row w-full h-full bg-gradient-to-r from-blue-400/30 bg-violet-400/30 dark:from-blue-600/30 dark:to-violet-900/30 rounded-3xl p-0'>
+              <div
+                key={project.name}
+                className={`h-full w-full absolute transition-all duration-500 ${
+                  selected === index
+                    ? "opacity-100"
+                    : `translate-x-full opacity-0`
+                }`}
+              >
+                <div className="flex flex-col lg:flex-row w-full h-full bg-gradient-to-r from-blue-400/30 bg-violet-400/30 dark:from-blue-600/30 dark:to-violet-900/30 rounded-3xl p-0">
                   <ImageCarousel images={project.images} />
-                  <div className='flex flex-col justify-between flex-1 text-left p-4 md:p-8'>
+                  <div className="flex flex-col justify-between flex-1 text-left p-4 lg:p-8">
                     <div>
-                      <h3 className='text-gray-600 dark:text-white font-bold text-md lg:text-xl mb-1'>{project.name}</h3>
-                      <p className='text-gray-600 dark:text-white text-xs lg:text-sm w-full overflow-clip'>{project.intro}</p>
-                      {project.techStack.frameworks &&
+                      <h3 className="text-gray-600 dark:text-white font-bold text-md lg:text-xl mb-1">
+                        {project.name}
+                      </h3>
+                      <p className="text-gray-600 dark:text-white text-xs lg:text-sm w-full overflow-clip">
+                        {project.intro}
+                      </p>
+                      {project.techStack.frameworks && (
                         <>
-                          <h4 className='text-gray-600 dark:text-white font-bold text-sm lg:text-md mt-1 mb-0'>Frameworks/Libraries used:</h4>
-                          <p className='text-gray-600 dark:text-white text-xs lg:text-sm w-full overflow-clip'>{project.techStack.frameworks}</p>
-                        </>}
-                      {project.techStack.languages &&
+                          <h4 className="text-gray-600 dark:text-white font-bold text-sm lg:text-md mt-1 mb-0">
+                            Frameworks/Libraries used:
+                          </h4>
+                          <p className="text-gray-600 dark:text-white text-xs lg:text-sm w-full overflow-clip">
+                            {project.techStack.frameworks}
+                          </p>
+                        </>
+                      )}
+                      {project.techStack.languages && (
                         <>
-                          <h4 className='text-gray-600 dark:text-white font-bold text-sm lg:text-md mt-1 mb-0'>Languages used:</h4>
-                          <p className='text-gray-600 dark:text-white text-xs lg:text-sm w-full overflow-clip'>{project.techStack.languages}</p>
-                        </>}
-                      {project.techStack.database &&
+                          <h4 className="text-gray-600 dark:text-white font-bold text-sm lg:text-md mt-1 mb-0">
+                            Languages used:
+                          </h4>
+                          <p className="text-gray-600 dark:text-white text-xs lg:text-sm w-full overflow-clip">
+                            {project.techStack.languages}
+                          </p>
+                        </>
+                      )}
+                      {project.techStack.database && (
                         <>
-                          <h4 className='text-gray-600 dark:text-white font-bold text-sm lg:text-md mt-1 mb-0'>Database used:</h4>
-                          <p className='text-gray-600 dark:text-white text-xs lg:text-sm w-full overflow-clip'>{project.techStack.database}</p>
-                        </>}
+                          <h4 className="text-gray-600 dark:text-white font-bold text-sm lg:text-md mt-1 mb-0">
+                            Database used:
+                          </h4>
+                          <p className="text-gray-600 dark:text-white text-xs lg:text-sm w-full overflow-clip">
+                            {project.techStack.database}
+                          </p>
+                        </>
+                      )}
                     </div>
-                    {project.demo ? <a href={project.demo} target='_blank' className="flex flex-row self-end items-center bg-gradient-to-t lg:bg-gradient-to-r from-blue-500 to-violet-500 duration-500 px-4 py-2 rounded-xl ml-8 text-white">Demo <HiExternalLink className='ml-2' /></a> : <div />}
+                    {project.demo ? (
+                      <a
+                        href={project.demo}
+                        target="_blank"
+                        className="flex flex-row self-end items-center bg-gradient-to-t lg:bg-gradient-to-r from-blue-500 to-violet-500 duration-500 px-4 py-2 rounded-xl ml-8 text-white"
+                      >
+                        Demo <HiExternalLink className="ml-2" />
+                      </a>
+                    ) : (
+                      <div />
+                    )}
                   </div>
                 </div>
               </div>
@@ -60,27 +98,68 @@ export default function PortfolioSection() {
           </div>
           <div className="absolute z-30 flex space-x-3 -translate-x-1/2 left-1/2 bottom-4">
             {portfolio.projects.map((_, index) => (
-              <button key={index} type="button" className={`w-3 h-3 rounded-full ${selected === index ? 'bg-indigo-500/60' : 'bg-gray-400/40'} hover:bg-indigo-500`} onClick={() => setSelected(index)}></button>
+              <button
+                key={index}
+                type="button"
+                className={`w-3 h-3 rounded-full ${
+                  selected === index ? "bg-indigo-500/60" : "bg-gray-400/40"
+                } hover:bg-indigo-500`}
+                onClick={() => setSelected(index)}
+              ></button>
             ))}
           </div>
-          <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full pl-2 cursor-pointer group focus:outline-none" onClick={onPrevious}>
-            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-200/80 dark:bg-gray-800/30 group-hover:bg-gray-200 dark:group-hover:bg-gray-800/60 group-focus:outline-none">
-              <svg className="w-4 h-4 text-indigo-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
-              </svg>
-              <span className="sr-only">Previous</span>
-            </span>
-          </button>
-          <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full pr-2 cursor-pointer group focus:outline-none" onClick={onNext}>
-            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-200/80 dark:bg-gray-800/30 group-hover:bg-gray-200 dark:group-hover:bg-gray-800/60 group-focus:outline-none">
-              <svg className="w-4 h-4 text-indigo-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
-              </svg>
-              <span className="sr-only">Next</span>
-            </span>
-          </button>
+          <div className="absolute top-0 right-0 h-full w-full flex justify-between items-center pointer-events-auto">
+            <button
+              className="pl-2 cursor-pointer group focus:outline-none"
+              type="button"
+              onClick={onPrevious}
+            >
+              <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-indigo-800/20 dark:bg-indigo-200/20 group-hover:bg-indigo-800/30 dark:group-hover:bg-indigo-200/30 group-focus:outline-none">
+                <svg
+                  className="w-4 h-4 text-indigo-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 1 1 5l4 4"
+                  />
+                </svg>
+                <span className="sr-only">Previous</span>
+              </span>
+            </button>
+            <button
+              type="button"
+              className="pr-2 cursor-pointer group focus:outline-none"
+              onClick={onNext}
+            >
+              <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-indigo-800/20 dark:bg-indigo-200/20 group-hover:bg-indigo-800/30 dark:group-hover:bg-indigo-200/30 group-focus:outline-none">
+                <svg
+                  className="w-4 h-4 text-indigo-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+                <span className="sr-only">Next</span>
+              </span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
